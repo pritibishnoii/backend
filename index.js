5bbbba107a71d132111a4cbc90d1a800e81e016e
priti <priti12bishnoi@gmail.com>
2024-10-01 15:12:39 +0530

    **

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const db = require('./dbConnection')
 const cors = require("cors");
 dotenv.config();
+const db = require("./dbConnection")
 
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/post");
@@ -12,22 +13,13 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-// app.use(cors("*"));
-
-app.use(
-	cors({
-		origin: "*",
-		credentials: true,
-	})
-);
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set("view engine", "ejs");
 
 db()
-
-
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
@@ -36,11 +28,13 @@ app.get("/", async (req, res) => {
   res.status(200).json("Server is up and running");
 });
 
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Internal Server Error" });
 });
 
-app.listen(process.env.PORT ||4000, () => {
+app.listen(process.env.PORT ||4000 , () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  
 });
